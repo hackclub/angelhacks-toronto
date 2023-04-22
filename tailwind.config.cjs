81ff8f33d56eb4ec042010a6f261a2f9f98e7397
Sam Liu <hello@samliu.dev>
2023-04-22 12:20:00 -0400

    Theme and style setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      teal: "#1F9AAA",
+      "dark-blue": "#196694",
+      turquoise: "#35CEBF",
+      green: "#68D997",
+      "light-green": "#C5E691",
+      orange: "#FDB32B",
+      "light-orange": "#FEDB8E",
+      red: "#FA5D63",
+      black: "#222222",
+      "sky-blue": "#8EDDFD",
+      "pale-blue": "#8492A5",
+      "black-blue": "#3C4857",
+      "pale-blue-2": "#6A798E",
+      white: "#FFFFFF",
+    },
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
